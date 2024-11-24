33d8866fdf08925a9caa2337988c094e1b730ccd
sakadume masato <namonakisizin@gmail.com>
2024-11-24 19:57:01 +0900

    [add] first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,65 @@
-import Link from "next/link";
-import Layout from "../components/Layout";
+import { GetServerSideProps, NextPage } from "next";
+import { useState } from "react";
+import styles from "./index.module.css";
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">About</Link>
-    </p>
-  </Layout>
-);
+type Props = {
+  initialImageUrl: string
+}
+
+const IndexPage: NextPage<Props> = ({ initialImageUrl }) => {
+  const [imageUrl,setImageUrl] = useState(initialImageUrl);
+  const [loading, setLoading] = useState(false);
+  
+  const handleClick = async() => {
+    setLoading(true);
+    const newImage = await fetchImage();
+    setImageUrl(newImage.url); // 画像URLの状態を更新する
+    setLoading(false); // 読込中フラグを倒す
+  }
+
+  return (
+          <div className={styles.page}>
+            <button onClick={handleClick} className={styles.button}>他のにゃんこも見る</button>
+            <div className={styles.frame}>{loading || <img src={imageUrl} className={styles.img}/>}</div>
+          </div>
+  );
+};
 
 export default IndexPage;
+
+export const getServerSideProps: GetServerSideProps<Props> = async() => {
+  const image = await fetchImage();
+  return {
+    props: {
+      initialImageUrl: image.url,
+    },
+  }
+}
+
+type Image = {
+  url: string;
+};
+
+const fetchImage = async (): Promise<Image> => {
+  const res = await fetch("https://api.thecatapi.com/v1/images/search");
+  const images:unknown = await res.json();
+
+  if (!Array.isArray(images)) {
+    throw new Error('猫の画像が取得できませんでした。');
+  }
+
+  const image: unknown = images[0];
+
+  if (!isImage(image)) {
+    throw new Error('猫の画像が取得できませんでした。')
+  }
+  return image;
+}
+
+const isImage = (value: unknown): value is Image => {
+  if(!value || typeof value !== 'object') {
+    return false;
+  }
+
+  return 'url' in value && typeof value.url === 'string'
+}
